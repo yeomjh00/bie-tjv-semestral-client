15365cb327c787b12718b95b5d9b4cb55480427a
James Yeom <yeomjaeh@cvut.cz>
2024-01-04 15:03:14 +0100

    feat: handler for 404, 400 HTTP Status

--- src/main/java/spring_client/semestral/exception/BadRequestHandler.java
+++ src/main/java/spring_client/semestral/exception/BadRequestHandler.java
@@ -4,14 +4,29 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
 
 @Slf4j
 @ControllerAdvice
 public class BadRequestHandler {
 
+    @ExceptionHandler(value = HttpClientErrorException.NotFound.class)
+    public String NotFound(Model model, HttpClientErrorException.NotFound e){
+        log.info("Custom 404 error with HttpClientErrorException.NotFound : {}", e.getMessage());
+        model.addAttribute("errorMessage", e.getMessage());
+        return "/error";
+    }
+
+    @ExceptionHandler(value = HttpClientErrorException.BadRequest.class)
+    public String BadRequest(Model model, HttpClientErrorException.BadRequest e){
+        log.info("Custom 400 error with HttpClientErrorException.BadRequest : {}", e.getMessage());
+        model.addAttribute("errorMessage", e.getMessage());
+        return "/error";
+    }
+
     @ExceptionHandler(value = IllegalArgumentException.class)
     public String IllegalArgument(Model model, IllegalArgumentException e){
-        log.error("400 error with IllegalArgument : {}", e.getMessage());
+        log.info("400 error with IllegalArgument : {}", e.getMessage());
         model.addAttribute("errorMessage", e.getMessage());
         return "/error";
     }
